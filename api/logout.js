6ab2f2bbb72cc8dd0122326b54bf8910356fe10e
anmixxan <antn.mkhv@gmail.com>
2026-03-23 05:12:54 +0300

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,1 +1,15 @@
+function clearCookie(name) {
+  return [
+    `${name}=`,
+    'Path=/',
+    'HttpOnly',
+    'Secure',
+    'SameSite=Lax',
+    'Max-Age=0'
+  ].join('; ');
+}
 
+export default async function handler(req, res) {
+  res.setHeader('Set-Cookie', clearCookie('dlynee_session'));
+  return res.status(200).json({ ok: true });
+}
